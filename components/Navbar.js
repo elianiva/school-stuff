9618cfbcf3a1388113a1c93aac0576f7f7953d9e
elianiva <dicha.arkana03@gmail.com>
2020-07-17 15:20:02 +0700

    added Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,43 @@
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          <div>
+            <Link href="/">
+              <a className="link">Posts</a>
+            </Link>
+          </div>
+          <div>
+            <Link href="/category">
+              <a className="link">Category</a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <style jsx>{`
+        nav {
+          border-bottom: 0.125rem #d4e1ef solid;
+          margin-bottom: 2rem;
+          padding: 1rem;
+        }
+        .container {
+          display: flex;
+          max-width: 800px;
+          margin: 0 auto;
+        }
+        .container div {
+          flex: 1;
+          text-align: center;
+        }
+        .link {
+          color: #292d3e;
+          text-decoration: none;
+          font-weight: 500;
+        }
+      `}</style>
+    </>
+  )
+}
