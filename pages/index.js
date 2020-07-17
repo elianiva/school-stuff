b9432c5cf6fda80004a2e44ad85e776ccc36e7cf
elianiva <dicha.arkana03@gmail.com>
2020-07-17 15:20:54 +0700

    added example homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,48 @@
-import Head from "next/head"
+import Layout from "../components/Layout"
+import Card from "../components/Card"
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>School Log</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main></main>
-    </div>
+    <Layout title="All Posts">
+      <div className="container">
+        <h1>All posts</h1>
+        <hr />
+        <Card
+          title="Rangkuman Basis Data"
+          desc="This is a short desc of something that would go here like lorem ipsum for example. But lorem ipsum is used to frequently so I use this made up phrase instead. On a second thought, making this phrase long enough is quite challenging..."
+          date="12 Juli 2020"
+          category="Basis Data"
+          slug="/something"
+        />
+      </div>
+      <style jsx>{`
+        .container {
+          margin: 0 auto;
+          max-width: 80ch;
+        }
+
+        h1 {
+          color: #292d3e;
+          text-align: center;
+          font-family: "PT Sans", Arial, sans-serif;
+        }
+
+        hr {
+          width: 10rem;
+          background-color: #5c90ff;
+          height: 0.25rem;
+          border-radius: 0.5rem;
+          border: none;
+          margin: 0.5rem auto 1.5rem;
+        }
+      `}</style>
+      <style jsx global>{`
+        * {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+      `}</style>
+    </Layout>
   )
 }
