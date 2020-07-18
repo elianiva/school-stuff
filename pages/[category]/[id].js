3c274c9c4c5ec7b88f8ceaf4aab5b431512b058e
elianiva <dicha.arkana03@gmail.com>
2020-07-18 17:26:16 +0700

    added basic content generation

--- pages/[category]/[id].js
+++ pages/[category]/[id].js
@@ -4,7 +4,41 @@ import { getAllPostsSlug, getPostData } from "../../lib/posts"
 export default function Post({ postData }) {
   return (
     <>
-      <Layout>{postData.title}</Layout>
+      <Layout title={postData.title}>
+        <div className="container">
+          <h1>{postData.title}</h1>
+          <hr />
+          <div
+            className="content"
+            dangerouslySetInnerHTML={{ __html: postData.body }}
+          ></div>
+        </div>
+      </Layout>
+      <style jsx>{`
+        .container {
+          max-width: 80ch;
+          margin: 0 auto;
+        }
+        h1 {
+          font-family: "PT Sans", Arial, sans-serif;
+          text-align: center;
+          font-size: 1.75rem;
+          line-height: 2rem;
+          margin: 0;
+        }
+        hr {
+          width: 10rem;
+          background-color: #5c90ff;
+          height: 0.25rem;
+          border-radius: 0.5rem;
+          border: none;
+          margin: 0.5rem auto 1.5rem;
+        }
+        .content {
+          font-size: 1.25rem;
+          line-height: 1.75rem;
+        }
+      `}</style>
     </>
   )
 }
@@ -18,7 +52,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.category, params.id)
+  const postData = await getPostData(params.category, params.id)
   return {
     props: {
       postData
