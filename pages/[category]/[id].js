90fbe6e065fe16f62a0e631acbdd02c34206acbd
elianiva <dicha.arkana03@gmail.com>
2020-07-19 21:38:09 +0700

    added post template styling

--- pages/[category]/[id].js
+++ pages/[category]/[id].js
@@ -7,38 +7,42 @@ export default function Post({ postData }) {
       <Layout title={postData.title}>
         <div className="container">
           <h1>{postData.title}</h1>
+          <span>{postData.date}</span>
           <hr />
           <div
             className="content"
             dangerouslySetInnerHTML={{ __html: postData.body }}
           ></div>
         </div>
+        <style jsx>{`
+          .container {
+            max-width: 80ch;
+            margin: 0 auto;
+          }
+          h1 {
+            font-family: "PT Sans", Arial, sans-serif;
+            text-align: center;
+            font-size: 1.75rem;
+            line-height: 2rem;
+            margin: 0;
+            color: #292d3e;
+          }
+          hr {
+            background-color: #5c90ff;
+            height: 0.125rem;
+            border-radius: 0.5rem;
+            border: none;
+            margin: 0.5rem auto 1.5rem;
+          }
+          span {
+            text-align: center;
+            display: block;
+            color: #ababba;
+            font-weight: 600;
+            font-family: "Open Sans", sans-serif;
+          }
+        `}</style>
       </Layout>
-      <style jsx>{`
-        .container {
-          max-width: 80ch;
-          margin: 0 auto;
-        }
-        h1 {
-          font-family: "PT Sans", Arial, sans-serif;
-          text-align: center;
-          font-size: 1.75rem;
-          line-height: 2rem;
-          margin: 0;
-        }
-        hr {
-          width: 10rem;
-          background-color: #5c90ff;
-          height: 0.25rem;
-          border-radius: 0.5rem;
-          border: none;
-          margin: 0.5rem auto 1.5rem;
-        }
-        .content {
-          font-size: 1.25rem;
-          line-height: 1.75rem;
-        }
-      `}</style>
     </>
   )
 }
